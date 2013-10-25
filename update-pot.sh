0b61d3baa42064e94529f831fff807c4b57a0972
Tony George <teejee2008@gmail.com>
2013-10-25 10:20:44 +0530

    v1.2

--- update-pot.sh
+++ update-pot.sh
@@ -6,7 +6,7 @@ cd "$DIR"
 
 echo "Updating translation templates..."
 
-xgettext --language=C --keyword=_ --copyright-holder='Tony George (teejee2008@gmail.com)' --package-name='timeshift' --package-version='1.1' --msgid-bugs-address='teejee2008@gmail.com' --escape --sort-output -o timeshift.pot src/*.vala
+xgettext --language=C --keyword=_ --copyright-holder='Tony George (teejee2008@gmail.com)' --package-name='timeshift' --package-version='1.2' --msgid-bugs-address='teejee2008@gmail.com' --escape --sort-output -o timeshift.pot src/*.vala
 
 #check for errors
 if [ $? -ne 0 ]; then

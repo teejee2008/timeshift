7ebbc7690c74db21b94e4cc3c73db0a6a0829dd5
Tony George <tony.george.kol@gmail.com>
2014-08-03 14:44:53 +0530

    Updated install script; Added uninstaller

--- installer/install.sh
+++ installer/install.sh
@@ -1,25 +1,163 @@
 #!/bin/bash
 
-backup=`pwd`
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-cd "$DIR"
+app_name='timeshift'
+app_fullname='Timeshift'
 
-echo "Installing files..."
+generic_depends=(rsync libgee json-glib)
+debian_depends=(rsync libgee2 libjson-glib-1.0-0 gksu libblkid1)
+redhat_depends=(rsync libgee json-glib)
+arch_depends=(rsync libgee json-glib)
 
-sudo cp -dpr --no-preserve=ownership -t / ./*
+generic_recommends=()
+debian_recommends=()
+redhat_recommends=()
+arch_recommends=()
+
+Reset='\e[0m'
+Red='\e[1;31m'
+Green='\e[1;32m'
+Yellow='\e[1;33m'
+
+CHECK_COLOR_SUPPORT() {
+	colors=`tput colors`
+	if [ $colors -gt 1 ]; then
+		COLORS_SUPPORTED=0
+	else
+		COLORS_SUPPORTED=1
+	fi
+}
+
+MSG_INFO() {
+	if [ $COLORS_SUPPORTED -eq 0 ]; then
+        echo -e "[${Yellow}*${Reset}] ${Green}$1${Reset}"
+    else
+        echo -e "[*] $1"
+    fi
+}
+
+MSG_WARNING() {
+	if [ $COLORS_SUPPORTED -eq 0 ]; then
+        echo -e "[${Red}!${Reset}] ${Yellow}$1${Reset}"
+    else
+        echo -e "[!] $1"
+    fi
+}
+
+MSG_ERROR() {
+    if [ $COLORS_SUPPORTED -eq 0 ]; then
+        echo -e "[${Red}X${Reset}] ${Yellow}$1${Reset}"
+    else
+        echo -e "[X] $1"
+    fi
+}
+
+CD_PUSH() {
+	cd_backup=`pwd`
+}
+
+CD_POP() {
+	if [ ! -z "${cd_backup}" ]; then
+		cd "${cd_backup}"
+	fi
+}
+
+EXIT(){
+	CD_POP
+	exit $1
+}
+
+WAIT_FOR_INPUT() {
+	echo ""
+	echo "Press any key to exit..."
+	read dummy
+}
+
+GET_SCRIPT_PATH(){
+	SCRIPTPATH="$(cd "$(dirname "$0")" && pwd)"
+	SCRIPTNAME=`basename $0`
+}
+
+RUN_AS_ADMIN() {
+	if [ ! `id -u` -eq 0 ]; then
+		GET_SCRIPT_PATH
+		if command -v sudo >/dev/null 2>&1; then
+			sudo "${SCRIPTPATH}/${SCRIPTNAME}"
+			EXIT $?
+		elif command -v su >/dev/null 2>&1; then
+			su -c "${SCRIPTPATH}/${SCRIPTNAME}"
+			EXIT $?
+		else
+			echo ""
+			MSG_ERROR "** Installer must be run as Admin (using 'sudo' or 'su') **"
+			echo ""
+			EXIT 1
+		fi
+	fi
+}
+
+CD_PUSH
+CHECK_COLOR_SUPPORT
+RUN_AS_ADMIN
+
+MSG_INFO "Expanding directories..."
+for f in `find ./ -type d -exec echo {} \;`; do
+    directory=`echo "$f" | sed -r 's/^.{2}//'`
+    mkdir -p -m 755 "/$directory"
+    echo "/$directory"
+done
+echo ""
+
+MSG_INFO "Installing files..."
+for f in `find ./ -type f \( ! -iname "install.sh" \) -exec echo {} \;`; do
+    file=`echo "$f" | sed -r 's/^.{2}//'`
+    install -m 0755 "./$file" "/$file"
+    echo "/$file"
+done
+echo ""
+
+if [ -f /etc/debian_version ]; then
+	if command -v apt-get >/dev/null 2>&1; then
+		MSG_INFO "Installing Debian packages..."
+		echo ""
+		for i in "${debian_depends[@]}"; do
+		  MSG_INFO "Installing: $i"
+		  apt-get -y install $i
+		done
+	fi
+elif [ -f /etc/redhat-release ]; then
+	if command -v yum >/dev/null 2>&1; then
+		MSG_INFO "Installing RedHat packages..."
+		echo ""
+		for i in "${redhat_depends[@]}"; do
+		  MSG_INFO "Installing: $i"
+		  yum -y install $i
+		done
+	fi
+elif [ -f /etc/arch-release ] || [ -f /etc/manjaro-release ]; then
+	if command -v pacman >/dev/null 2>&1; then
+		MSG_INFO "Installing ArchLinux packages..."
+		echo ""
+		for i in "${arch_depends[@]}"; do
+		  MSG_INFO "Installing: $i"
+		  pacman -S --noconfirm $i
+		done
+	fi
+fi
+echo ""
 
 if [ $? -eq 0 ]; then
-	echo "Installed successfully."
+	MSG_INFO "Installed successfully."
 	echo ""
-	echo "Start TimeShift using the shortcut in the application menu"
-	echo "or by running the command: sudo timeshift"	
-	echo ""
-	echo "Following packages are required for TimeShift to function correctly:"
-	echo "- libgtk-3 libgee2 libsoup libjson-glib rsync"
-	echo "Please ensure that these packages are installed and up-to-date"
+	MSG_INFO "Start ${app_fullname} using the shortcut in the Applications Menu"
+	MSG_INFO "or by running the command: sudo ${app_name}"	
+	MSG_INFO "If it fails to start, check and install following packages:"
+	MSG_WARN "Required: ${generic_depends[@]}"
+	#MSG_WARN "Optional: ${generic_recommends[@]}"
+	WAIT_FOR_INPUT
+	EXIT 0
 else
-	echo "Installation failed!"
-	exit 1
+	MSG_ERROR "Install completed with errors"
+	EXIT 1
 fi
 
-cd "$backup"
+CD_POP
